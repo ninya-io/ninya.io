71aaf1ce12887ea099a9d3fabd994d3b0b66c251
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2013-12-07 23:08:25 +0100

    fix(searchService): limit response to max 100 rows

--- server/searchService.js
+++ server/searchService.js
@@ -8,7 +8,7 @@ var SearchService = function(){
         var deferred = Q.defer();
 
         var sql = 'SELECT * FROM users WHERE ';
-        var sqlSuffix = ' ORDER By ("user"->>\'reputation\')::int DESC';
+        var sqlSuffix = ' ORDER By ("user"->>\'reputation\')::int DESC LIMIT 100';
 
         var answerTagsCount = 0;
         var answerTagsSql = '';
